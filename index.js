5836fbf3e6bcbf94d446f177126e44e7320248da
leandro.pereira <leandro.pereira@fepi.br>
2025-07-24 15:25:19 -0300

    feat: criar itens de lista dinâmicos com checkboxes

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const inputItem = document.getElementById("input-item")
+const inputItem = document.getElementById("input-item");
+const listaDeCompras = document.getElementById("lista-de-compras");
+const botaoAdicionar = document.getElementById("adicionar-item");
 
-const botaoAdicionar = document.getElementById("adicionar-item")
+let contador = 0;
 
 botaoAdicionar.addEventListener("click", (evento) => {
     evento.preventDefault();
@@ -8,9 +10,20 @@ botaoAdicionar.addEventListener("click", (evento) => {
     if(inputItem.value === "") {
         alert("Por favor, insira um item válido.");
         return;
-    } else {
+    }
         const itemDaLista = document.createElement("li");
         const containerItemDaLista = document.createElement("div");
         containerItemDaLista.classList.add("lista-item-container");
-    }
+        const inputCheckbox = document.createElement("input");
+        inputCheckbox.type = "checkbox";        
+        inputCheckbox.id = "checkbox-" + contador++; 
+        const nomeItem = document.createElement("p");
+        nomeItem.textContent = inputItem.value;
+
+        containerItemDaLista.appendChild(inputCheckbox);
+        containerItemDaLista.appendChild(nomeItem);
+        itemDaLista.appendChild(containerItemDaLista);
+
+        listaDeCompras.appendChild(itemDaLista);
+        inputItem.value = ""; // Limpa o campo de entrada após adicionar o item    
 })
