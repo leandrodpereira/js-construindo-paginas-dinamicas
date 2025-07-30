6ad2ca229d0bedd0e59c3a37541fe2306d78e6c7
Leandro <leandro.pereira@fepi.br>
2025-07-30 17:01:02 -0300

    refactor: modularização parte 1

--- index.js
+++ index.js
@@ -1,56 +1,18 @@
-const inputItem = document.getElementById("input-item");
+import { criarItemDaLista } from "./scripts/criarItemDaLista.js";
+
+
 const listaDeCompras = document.getElementById("lista-de-compras");
 const botaoAdicionar = document.getElementById("adicionar-item");
 
 let contador = 0;
 
 botaoAdicionar.addEventListener("click", (evento) => {
-    evento.preventDefault();
-    
-    if(inputItem.value === "") {
-        alert("Por favor, insira um item válido.");
-        return;
-    }
-        const itemDaLista = document.createElement("li");
-        const containerItemDaLista = document.createElement("div");
-        containerItemDaLista.classList.add("lista-item-container");
-        const inputCheckbox = document.createElement("input");
-        inputCheckbox.type = "checkbox";        
-        inputCheckbox.id = "checkbox-" + contador++; 
-        const nomeItem = document.createElement("p");
-        nomeItem.textContent = inputItem.value;
+           
+    const itemDaLista = criarItemDaLista();
 
-        inputCheckbox.addEventListener("click", () => {
-            if (inputCheckbox.checked) {
-                itemDaLista.style.textDecoration = "line-through";
-            } else {                
-                itemDaLista.style.textDecoration = "none";
-            }
-        });
-
-        containerItemDaLista.appendChild(inputCheckbox);
-        containerItemDaLista.appendChild(nomeItem);
-        itemDaLista.appendChild(containerItemDaLista);
-
-        const diaDaSemana = new Date().toLocaleDateString('pt-BR', { 
-            weekday: 'long' });
-        const data = new Date().toLocaleDateString('pt-BR');
-        
-        const hora = new Date().toLocaleTimeString('pt-BR', {
-            hour: 'numeric',
-            minute: 'numeric'
-        });
-
-        const dataCompleta = `${diaDaSemana}, (${data}) às ${hora}`;//Template String
-        const dataHora = document.createElement("p");
-        dataHora.classList.add("texto-data");
-        dataHora.innerText = dataCompleta;
-
-        itemDaLista.appendChild(dataHora);
-
-        listaDeCompras.appendChild(itemDaLista);
-        inputItem.value = ""; // Limpa o campo de entrada após adicionar o item   
-        atualizarMensagemListaVazia();       
+    listaDeCompras.appendChild(itemDaLista);
+    inputItem.value = ""; // Limpa o campo de entrada após adicionar o item   
+    atualizarMensagemListaVazia();       
 })
 
 
---
+++ scripts/criarItemDaLista.js
@@ -0,0 +1,46 @@
+ const inputItem = document.getElementById("input-item");
+ 
+ export function criarItemDaLista() {   
+    evento.preventDefault(); 
+    if(inputItem.value === "") {
+        alert("Por favor, insira um item válido.");
+        return;
+    }
+        const itemDaLista = document.createElement("li");
+        const containerItemDaLista = document.createElement("div");
+        containerItemDaLista.classList.add("lista-item-container");
+        const inputCheckbox = document.createElement("input");
+        inputCheckbox.type = "checkbox";        
+        inputCheckbox.id = "checkbox-" + contador++; 
+        const nomeItem = document.createElement("p");
+        nomeItem.textContent = inputItem.value;
+
+        inputCheckbox.addEventListener("click", () => {
+            if (inputCheckbox.checked) {
+                itemDaLista.style.textDecoration = "line-through";
+            } else {                
+                itemDaLista.style.textDecoration = "none";
+            }
+        });
+
+        containerItemDaLista.appendChild(inputCheckbox);
+        containerItemDaLista.appendChild(nomeItem);
+        itemDaLista.appendChild(containerItemDaLista);
+
+        const diaDaSemana = new Date().toLocaleDateString('pt-BR', { 
+            weekday: 'long' });
+        const data = new Date().toLocaleDateString('pt-BR');
+        
+        const hora = new Date().toLocaleTimeString('pt-BR', {
+            hour: 'numeric',
+            minute: 'numeric'
+        });
+
+        const dataCompleta = `${diaDaSemana}, (${data}) às ${hora}`;//Template String
+        const dataHora = document.createElement("p");
+        dataHora.classList.add("texto-data");
+        dataHora.innerText = dataCompleta;
+        itemDaLista.appendChild(dataHora);
+
+        return itemDaLista;
+}
